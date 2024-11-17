4999a8b1080b065f5ed76934db5810ffe5985334
Florrison <florian_micklich@gmx.de>
2024-11-17 14:45:54 +0100

    replacingCode: pokemonTypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,1 @@
-alert('Hello world');
-
-let favoriteFood = 'Sushi!';
-document.write(favoriteFood);
+let pokemon=[{name: "ivysaur", height: 7, type: ['grass','poison']}, {name: "squirtle", height: 5, type: ['water','ice']}, {name: "charizard", height: 17, type: ['fire','air']}];
