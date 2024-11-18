310f39fe9a1c77ca192df606d498d77d4432c433
Florrison <florian_micklich@gmx.de>
2024-11-18 17:43:20 +0100

    pokemonListnHeight

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-let pokemon=[{name: "ivysaur", height: 7, type: ['grass','poison']}, {name: "squirtle", height: 5, type: ['water','ice']}, {name: "charizard", height: 17, type: ['fire','air']}];
+let pokemonList = [
+    {name: 'Bulbasaur', height: '0.7', type: ['grass', 'poison']},
+    {name: 'Charizard', height: '1.7', type: ['fire', 'air']},
+    {name: 'Squirtle', height: '0.5', type: ['water']},
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    console.log('name:', pokemon.name);
+    console.log('height:', pokemon.height);
+    document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ')');
+    if (pokemon.height <= 0.5) {
+        document.write(" - Aww, that is tiny!");
+    }else if (pokemon.height >= 1) {
+        document.write(" - Wow, that is big!");
+    }else {
+        document.write(" - Hmm, kind of average.");
+    }
+}
